hot-fix wrong corner normalization when padding

engine.py
@@ -52,7 +52,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
         # print("Max #polys: ", max([len(gt_instances[i].gt_masks.polygons) for i in range(len(gt_instances))]))
         # print("Max #corners: ", max([gt_instances[i].gt_masks.polygons for i in range(gt_instances)]))
         if not poly2seq:
-            room_targets = pad_gt_polys(gt_instances, model_obj.num_queries_per_poly, device)
+            room_targets = pad_gt_polys(gt_instances, model_obj.num_queries_per_poly, samples[0].shape[1], device)
             outputs = model(samples)
         else:
             for key in batched_extras.keys():
@@ -116,7 +116,7 @@ def evaluate(model, criterion, dataset_name, data_loader, device, plot_density=F
         scene_ids = [x["image_id"]for x in batched_inputs]
         gt_instances = [x["instances"].to(device) for x in batched_inputs]
         if not poly2seq:
-            room_targets = pad_gt_polys(gt_instances, model_obj.num_queries_per_poly, device)
+            room_targets = pad_gt_polys(gt_instances, model_obj.num_queries_per_poly, samples[0].shape[1], device)
             outputs = model(samples)
         else:
             room_targets = batched_extras
@@ -293,7 +293,7 @@ def evaluate_v2(model, criterion, dataset_name, data_loader, device, plot_densit
         scene_ids = [x["image_id"]for x in batched_inputs]
         gt_instances = [x["instances"].to(device) for x in batched_inputs]
         if not poly2seq:
-            room_targets = pad_gt_polys(gt_instances, model_obj.num_queries_per_poly, device)
+            room_targets = pad_gt_polys(gt_instances, model_obj.num_queries_per_poly, samples[0].shape[1], device)
             outputs = model(samples)
         else:
             for key in batched_extras.keys():

util/poly_ops.py
@@ -40,7 +40,7 @@ def get_all_order_corners(corners):
     return all_corners
 
 
-def pad_gt_polys(gt_instances, num_queries_per_poly, device):
+def pad_gt_polys(gt_instances, num_queries_per_poly, image_size, device):
     """Pad the ground truth polygons so that they have a uniform length
     """
 
@@ -55,7 +55,7 @@ def pad_gt_polys(gt_instances, num_queries_per_poly, device):
 
         for i, poly in enumerate(gt_inst.gt_masks.polygons):
             corners = torch.from_numpy(poly[0]).to(device)
-            corners = torch.clip(corners, 0, 255) / 255
+            corners = torch.clip(corners, 0, image_size - 1) / (image_size - 1)
 
             # automatically skip the polygon if it is too long
             if len(corners) > thr_length: