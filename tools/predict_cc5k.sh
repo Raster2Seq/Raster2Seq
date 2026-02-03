1417a321d69f96f984ec209deb6bb09fe5671df7
hao-pt <tienhaophung@gmail.com>
2026-02-03 17:20:08 -0500

    indent

--- tools/predict_cc5k.sh
+++ tools/predict_cc5k.sh
@@ -5,18 +5,18 @@ FOLDER=test
 CKPT=cc5k_sem_res256_ep0499.pth
 
 python predict.py \
-               --dataset_name=cubicasa \
-               --dataset_root=${DATA}/${FOLDER} \
-               --checkpoint=${CKPT} \
-               --output_dir=cc5k_${FOLDER}_preds \
-               --semantic_classes=12 \
-               --input_channels 3 \
-               --poly2seq \
-               --seq_len 512 \
-               --num_bins 32 \
-               --disable_poly_refine \
-               --dec_attn_concat_src \
-               --per_token_sem_loss \
-               --use_anchor \
-               --ema4eval \
-               --save_pred
+    --dataset_name=cubicasa \
+    --dataset_root=${DATA}/${FOLDER} \
+    --checkpoint=${CKPT} \
+    --output_dir=cc5k_${FOLDER}_preds \
+    --semantic_classes=12 \
+    --input_channels 3 \
+    --poly2seq \
+    --seq_len 512 \
+    --num_bins 32 \
+    --disable_poly_refine \
+    --dec_attn_concat_src \
+    --per_token_sem_loss \
+    --use_anchor \
+    --ema4eval \
+    --save_pred
